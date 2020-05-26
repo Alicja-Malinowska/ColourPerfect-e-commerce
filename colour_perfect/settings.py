e535ec9cf4adec072bf91cdc3a8fbf91e49a67ea
Alicja <amalinowska.p@gmail.com>
2020-05-26 10:38:40 +0100

    new key generated and set as env var

--- colour_perfect/settings.py
+++ colour_perfect/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cs(0#3&85s)u@u=e)9%qa^dg+gkict8n(o4y2*vpo8dgx+k*7h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
