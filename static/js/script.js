0ce98a6b2d350925f7391f9088436a49d46e3af9
Alicja <amalinowska.p@gmail.com>
2020-05-30 15:23:42 +0100

    beauty test and product view styled

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,30 @@ $(document).ready(function () {
 
 });
 
+
+/**
+ * Allows increment and decrement product amount in range 0-99
+ */
+$(".quantity-button").on("click", function () {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("#quantity").val();
+
+    if ($button.text() == "+") {
+        if (oldValue < 99) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            newVal = 99;
+        }
+
+    } else {
+        if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+        } else {
+            newVal = 0;
+        }
+    }
+
+    $button.parent().find("#quantity").val(newVal);
+
+});
