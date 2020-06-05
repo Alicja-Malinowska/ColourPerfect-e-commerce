f2566c8e1cf63e485e49eca62592c5fac8fa02ec
Alicja <amalinowska.p@gmail.com>
2020-06-05 13:48:33 +0100

    colour assigning function written

---
+++ colour-conv.py
@@ -0,0 +1,35 @@
+from colour import Color
+
+
+def colour_match(hex_val):
+    c = Color(hex_val)
+    s = c.hsl[1]
+    l = c.hsl[2]
+    
+    if s <= 0.25:
+        if l > 0.3 :
+            return "summer"
+        else:
+            return "winter"
+    elif s >= 0.75:
+        if l <= 0.30 :
+            return "winter"
+        elif l <= 0.60:
+            return "autumn"
+        elif l <= 0.90:
+            return "spring"
+        else:
+            return "summer"
+    else:
+        if l <= 0.35:
+            return "winter"
+        elif l <= 0.5:
+            return "autumn"
+        elif l <= 0.7:
+            return "spring" 
+        else:
+            return "summer"
+
+print(colour_match("#ffcc00"))
+    
+    
