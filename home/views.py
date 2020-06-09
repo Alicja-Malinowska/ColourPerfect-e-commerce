41ec14fa4651f25a20ee9b3b3507af0f3ad569eb
Alicja <amalinowska.p@gmail.com>
2020-06-09 14:11:09 +0100

    brands and categories added to the menu

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from products.models import Category, Brand
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all().order_by('name')
+    brands = Brand.objects.all().order_by('name')
+
+
+    context = {
+        'categories': categories,
+        'brands': brands
+    }
+    return render(request, 'index.html', context)
