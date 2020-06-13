ec8306828f11d1ce092ea114eb65904e07c88453
Alicja <amalinowska.p@gmail.com>
2020-06-13 15:55:46 +0100

    selected individual products displayd together with their colours

--- products/views.py
+++ products/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 # Create your views here.
+def product(request, id):
+
+    SEASONS = ['Spring', 'Summer', 'Autumn', 'Winter']
+    product = get_object_or_404(Product, pk=id)
+    all_colours = product.product_colors.all()
+    brand = product.brand.all()
+    category = product.category.all()
+
+    colours = {}
+    
+    if all_colours:
+        for season in SEASONS:
+            season_colours = []
+            for colour in all_colours:
+                if colour.season == season.lower():
+                    season_colours.append(colour)
+            colours[season] = season_colours
+
+    has_colours = bool(colours)
+
+    context = {
+        'product': product,
+        'colours': colours,
+        'brand': brand,
+        'category': category,
+        'seasons': SEASONS,
+        'has_colours': has_colours,
+
+    }
+
+    return render(request, 'product.html', context)
