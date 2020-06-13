4ce8a8b5f85ec2b1545552fa2b981880f9352512
Alicja <amalinowska.p@gmail.com>
2020-06-13 11:11:37 +0100

    comments added

--- home/views.py
+++ home/views.py
@@ -5,12 +5,14 @@ from products.models import Category, Brand, Product
 
 
 def home(request):
+    ''' home view displays 3 suggested products for each beauty types, including the matching colour '''
     SEASONS = ['Spring', 'Summer', 'Autumn', 'Winter']
     suggestions = {}
     categories = Category.objects.all().order_by('name')
     brands = Brand.objects.all().order_by('name')
     
-
+    # create a suggestions dictionary, where keys are 4 seasons and values are lists of tuples 
+    # (product object, matching colour object)
     for season in SEASONS:
         season_products = Product.objects.filter(product_colors__season=season.lower()).prefetch_related('product_colors').distinct().order_by('description')[:3]
         col_prod = []
