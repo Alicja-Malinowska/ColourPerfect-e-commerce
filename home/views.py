0d358dba6c361cc7f63778e2651320a61cb666c4
Alicja <amalinowska.p@gmail.com>
2020-06-16 09:22:08 +0100

    get products function extracted for reuse

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from products.models import Category, Brand, Product
+from helpers.get_products_and_colours import get_products
 
 # Create your views here.
 
@@ -14,12 +15,8 @@ def home(request):
     # create a suggestions dictionary, where keys are 4 seasons and values are lists of tuples 
     # (product object, matching colour object)
     for season in SEASONS:
-        season_products = Product.objects.filter(product_colors__season=season.lower()).prefetch_related('product_colors').distinct().order_by('description')[:3]
-        col_prod = []
-        for product in season_products:
-            colour = product.product_colors.filter(season = season.lower()).last()
-            col_prod.append((product, colour))
-        suggestions[season] = col_prod
+        
+        suggestions[season] = get_products(season, 'description')
     
     
     context = {
