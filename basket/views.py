bf206068cd4823f6bc5a0d0ce9753da9ee19e2a1
Alicja <amalinowska.p@gmail.com>
2020-06-22 10:20:55 +0100

    adding to session basket fixed

--- basket/views.py
+++ basket/views.py
@@ -22,22 +22,22 @@ def basket(request):
             basket_products.append(basket_product)
             prod_sum = Decimal(item.product.price) * item.quantity
             total += prod_sum
-    else: 
+    else:
         basket = request.session.get('basket', {})
         basket_products = []
         total = 0
-        for product_id in basket:
-            print(basket[product_id])
+        for item_id in basket:
             basket_product = {}
-            prod_obj = get_object_or_404(Product, pk=product_id)
+            prod_obj = get_object_or_404(Product, pk=basket[item_id]['product'])
             basket_product['prod_obj'] = prod_obj
-            if not basket[product_id]['colour'] == None:
-                basket_product['colour'] = get_object_or_404(Colour, pk=basket[product_id]['colour'])
-            basket_product['quantity'] = basket[product_id]['quantity']
+            if not basket[item_id]['colour'] == None:
+                basket_product['colour'] = get_object_or_404(
+                    Colour, pk=basket[item_id]['colour'])
+            basket_product['quantity'] = basket[item_id]['quantity']
             basket_products.append(basket_product)
-            prod_sum = Decimal(prod_obj.price) * basket[product_id]['quantity']
+            prod_sum = Decimal(prod_obj.price) * basket[item_id]['quantity']
             total += prod_sum
-            
+
     context = {
         'products': basket_products,
         'total': total,
@@ -53,10 +53,11 @@ def add_to_basket(request, product_id):
     colour = request.POST.get('colour')
     colour_obj = None
     colour_name = 'clear or standard'
+    colour_id = None
     if Colour.objects.filter(hex_value=colour).exists():
-        colour_obj = Colour.objects.get(hex_value=colour)  
+        colour_obj = Colour.objects.get(hex_value=colour)
         colour_name = colour_obj.name
-    
+        colour_id = colour_obj.id
 
     # validate if colour should have been chosen for the producst or not
     # and if the colour value is correct
@@ -74,11 +75,11 @@ def add_to_basket(request, product_id):
                            ("This product is not available in this colour, please select a colour from the displayed ones"))
             return redirect(redirect_url)
     elif request.POST.get('has_colours') == 'False' and not colour == None:
-            messages.error(request,
-                           ("There are no colour options for this product."))
-            return redirect(redirect_url)
+        messages.error(request,
+                       ("There are no colour options for this product."))
+        return redirect(redirect_url)
 
-    #valdidate quantity
+    # valdidate quantity
     try:
         quantity = int(request.POST.get('quantity'))
 
@@ -88,52 +89,49 @@ def add_to_basket(request, product_id):
             return redirect(redirect_url)
     except:
         messages.error(request,
-                           ("Quantity must be an integer."))
+                       ("Quantity must be an integer."))
         return redirect(redirect_url)
 
-    #for logged-in users save basket in the database
+    # for logged-in users save basket in the database
     if request.user.is_authenticated:
         basket = Basket.objects.get_or_create(owner=request.user)[0]
         basket_items = BasketItem.objects.filter(basket=basket)
 
         if basket_items.filter(product__id=product.id, colour__hex_value=colour).exists():
-            basket_item = BasketItem.objects.get(product__id=product.id, colour__hex_value=colour)
+            basket_item = BasketItem.objects.get(
+                product__id=product.id, colour__hex_value=colour)
             basket_item.quantity += quantity
             basket_item.save()
             messages.success(request,
-                           (f"{quantity} more {product.name} in colour: {colour_name} added to the basket."))
-            
+                             (f"{quantity} more {product.name} in colour: {colour_name} added to the basket."))
+
         else:
-            basket_item = BasketItem(basket=basket, product=product, quantity=quantity, colour=colour_obj)
+            basket_item = BasketItem(
+                basket=basket, product=product, quantity=quantity, colour=colour_obj)
             basket_item.save()
             messages.success(request,
-                           (f"Added {quantity} {product.name} in colour: {colour_name} to the basket."))
+                             (f"Added {quantity} {product.name} in colour: {colour_name} to the basket."))
 
-    # if no user is logged in use session to store basket items   
-    else: 
-#######THINK TO REPLACE PRODUCT_ID AS BASKET KEYS TO UUID!!!!!!!!!!!!!!!!!!!!!
+    # if no user is logged in use session to store basket items
+    else:
         basket = request.session.get('basket', {})
-        try:
-            basket_colour = basket[product_id]['colour']
-        except:
-            basket_colour = False
+        item_id = str(product_id) + str(colour_id)
+       
 
-        if product_id in list(basket.keys()) and basket_colour == colour_obj.id:
-            basket[product_id]['quantity'] += quantity
+        if item_id in list(basket.keys()):
+            basket[item_id]['quantity'] += quantity
             messages.success(request,
-                        (f"{quantity} more {product.name} in colour: {colour_name} added to the basket."))
+                             (f"{quantity} more {product.name} in colour: {colour_name} added to the basket."))
         else:
-            basket[product_id] = {
+            basket[item_id] = {
+                'product': product_id,
                 'quantity': quantity,
-                'colour': None
+                'colour': colour_id
             }
-            
-            if not colour_obj == None:
-                basket[product_id]['colour'] = colour_obj.id
             messages.success(request,
-                           (f"Added {quantity} {product.name} in colour: {colour_name} to the basket."))
-        request.session['basket'] = basket
-    
-    return redirect(redirect_url)
-   
+                             (f"Added {quantity} {product.name} in colour: {colour_name} to the basket."))
 
+        request.session['basket'] = basket
+        print(basket)
+
+    return redirect(redirect_url)
