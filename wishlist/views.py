1c6db80fc5989780d987703c68d6a03f42feada4
Alicja <amalinowska.p@gmail.com>
2020-06-25 14:58:59 +0100

    adding to wishlist from product view via basket added

--- wishlist/views.py
+++ wishlist/views.py
@@ -25,8 +25,8 @@ def add_to_wishlist(request, product_id):
 #   THIS MIGHT NEED TO BE EXTRACTED AND USED HERE AND IN BASKET AS AN IMPORT 
     product = get_object_or_404(Product, pk=product_id)
     all_colours = product.product_colors.all()
-    redirect_url = request.POST.get('redirect_url')
-    colour = request.POST.get('colour')
+    redirect_url = request.POST.get('redirect_url') or request.session['redirect_url']
+    colour = request.POST.get('colour') or request.session['colour']
     colour_obj = None
     colour_name = 'clear or standard'
     colour_id = None
