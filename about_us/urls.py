3ec79bbd7d876914dcb827858658be7e278ac867
Alicja <amalinowska.p@gmail.com>
2020-06-26 11:54:21 +0100

    delivery link on footer takes user to delivery&returns section on the about page

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from about_us.views import about_us
+from about_us.views import about_us, delivery_returns
 
 
 urlpatterns = [
     path('', about_us, name='about_us'),
+    path('delivery', delivery_returns, name='delivery_returns'),
+
 
 ]
--- about_us/views.py
+++ about_us/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 def about_us(request):
 
     return render(request, 'about-us.html')
+
+def delivery_returns(request):
+
+    return redirect(reverse('about_us') + '#delivery-returns')
