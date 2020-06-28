d2aa15c7010d26a3a5d310a4219a7f4b9f2b0f84
Alicja <amalinowska.p@gmail.com>
2020-06-28 14:45:36 +0100

    checkout form prefilled if user has details saved in their profile

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from checkout.forms import OrderForm
+from profiles.forms import ProfileForm
 from basket.models import BasketItem, Basket
 from basket.contexts import basket_content
 from checkout.models import Order, OrderItem
@@ -16,7 +17,6 @@ def checkout(request):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
-        
 
         form_data = {
             'first_name': request.POST['first_name'],
@@ -32,20 +32,28 @@ def checkout(request):
         current_basket = basket_content(request)
         total = current_basket['total']
         order_form = OrderForm(form_data)
-        
+
         if order_form.is_valid():
             order = order_form.save()
             order.total = total
             order.save()
 
             if request.user.is_authenticated:
-                
-                basket_items = BasketItem.objects.filter(basket__owner=request.user)
+
+                basket_items = BasketItem.objects.filter(
+                    basket__owner=request.user)
                 for basket_item in basket_items:
-                    order_item = OrderItem(order=order, product=basket_item.product, quantity=basket_item.quantity, colour=basket_item.colour)
+                    order_item = OrderItem(order=order, product=basket_item.product,
+                                           quantity=basket_item.quantity, colour=basket_item.colour)
                     order_item.save()
-                
-            else: 
+
+                if 'save' in request.POST:
+                    profile = get_object_or_404(Profile, owner=request.user)
+                    profile_form = ProfileForm(form_data, instance=profile)
+                    if profile_form.is_valid():
+                        profile_form.save()
+
+            else:
                 basket = request.session.get('basket', {})
 
                 for item in basket:
@@ -55,13 +63,15 @@ def checkout(request):
                     if Colour.objects.filter(pk=basket[item]['colour']).exists():
                         colour = Colour.objects.get(pk=basket[item]['colour'])
 
-                    order_item = OrderItem(order=order, product=product, quantity=quantity, colour=colour)
+                    order_item = OrderItem(
+                        order=order, product=product, quantity=quantity, colour=colour)
                     order_item.save()
 
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'There is an error in your form, please check your data')
-   
+            messages.error(
+                request, 'There is an error in your form, please check your data')
+
     else:
 
         if request.user.is_authenticated:
@@ -69,11 +79,28 @@ def checkout(request):
                 basket = Basket.objects.get(owner=request.user)
             else:
                 basket = None
-        else: 
+
+            if Profile.objects.filter(owner=request.user).exists():
+                profile = Profile.objects.get(owner=request.user)
+
+                order_form = OrderForm(initial={
+                    'first_name': profile.first_name,
+                    'last_name': profile.first_name,
+                    'email_address': profile.email_address,
+                    'phone_number': profile.phone_number,
+                    'street_address1': profile.street_address1,
+                    'street_address2': profile.street_address2,
+                    'town_or_city': profile.town_or_city,
+                    'postcode': profile.postcode,
+                    'country': profile.country,
+                    })
+        else:
             basket = request.session.get('basket', {})
+            order_form = OrderForm()
 
         if not basket:
-            messages.error(request, "There's nothing in your basket at the moment")
+            messages.error(
+                request, "There's nothing in your basket at the moment")
             return redirect(reverse('basket'))
 
         current_basket = basket_content(request)
@@ -83,10 +110,9 @@ def checkout(request):
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
-            )
+        )
 
-
-    order_form = OrderForm()
+    
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
@@ -95,21 +121,22 @@ def checkout(request):
 
     return render(request, 'checkout.html', context)
 
+
 def checkout_success(request, order_number):
-    
+
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Success - all the fabulous colours are on their way to you! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email_address}.')
-    
+
     send_email(order)
 
     if request.user.is_authenticated:
-        
+
         profile = Profile.objects.get(owner=request.user)
         order.profile = profile
         order.save()
-        
+
         if BasketItem.objects.filter(basket__owner=request.user).exists():
 
             BasketItem.objects.filter(basket__owner=request.user).delete()
@@ -122,4 +149,4 @@ def checkout_success(request, order_number):
         'order': order,
     }
 
-    return render(request, 'checkout-success.html', context)   
+    return render(request, 'checkout-success.html', context)
