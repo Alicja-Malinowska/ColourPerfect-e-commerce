7d26107763cf415231168420dc42b5b4a97fc540
Alicja <amalinowska.p@gmail.com>
2020-07-04 16:45:46 +0100

    about us views tests written and pass

---
+++ about_us/tests_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestAboutUsViews(TestCase):
+
+    def test_get_about_page(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "about-us.html")
+    
+    def test_get_delivery_section(self):
+        response = self.client.get("/about/#delivery-returns")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "about-us.html")
