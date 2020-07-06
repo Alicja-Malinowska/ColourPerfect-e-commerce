f084c901d523c35caa834c9c7efaa1e4931223fa
Alicja <amalinowska.p@gmail.com>
2020-07-06 14:50:28 +0100

    models tests complete

---
+++ products/tests_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from products.models import Product, Colour, Category, Brand
+
+# Create your tests here.
+
+class TestProductsModels(TestCase):
+
+    def test_product_created_correctly(self):
+
+        c1 = Colour.objects.create(hex_value = '#ffffff', name = "colour1", season = "summer")
+        c2 = Colour.objects.create(hex_value = '#fbced7', name = "colour2", season = "summer")
+        category = Category.objects.create(name = "test_category", display_name = "Test Category")
+        brand = Brand.objects.create(name = "test_brand", display_name = "Test Brand")
+        product = Product.objects.create(brand = brand, name = "test_product", price = 5.00, image_link = 'www.test.com', description = "test desc")
+        product.category.add(category)
+        product.product_colors.add(c1, c2)
+
+        self.assertEqual(product.product_colors.all()[0].hex_value, "#ffffff")
+        self.assertEqual(product.category.all()[0].name, "test_category")
+        self.assertEqual(product.brand.name, "test_brand")
+        
+
