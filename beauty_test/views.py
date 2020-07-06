0ff252eebb9f6ab95259cf762d19e6bdb8def23b
Alicja <amalinowska.p@gmail.com>
2020-07-06 09:30:33 +0100

    beauty test tests complete

--- beauty_test/views.py
+++ beauty_test/views.py
@@ -30,7 +30,10 @@ def results(request):
     most_occurances = max([(result_list.count(season.lower()), season) for season in SEASONS])
     result = most_occurances[1]
 
+    # get 3 random products with colours matching the result season
     products = get_products(result, '?', request)
+
+    # get 12 random colours matching the result season
     colours = Colour.objects.filter(season=result.lower()).order_by('?')[:12]
     
     context = {
