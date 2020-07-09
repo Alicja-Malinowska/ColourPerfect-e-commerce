ca225557a6df9494071e12ed4a36bbb5d77da295
Alicja <amalinowska.p@gmail.com>
2020-07-09 12:15:48 +0100

    order number chars changed to 40

--- checkout/models.py
+++ checkout/models.py
@@ -5,7 +5,7 @@ from profiles.models import Profile
 
 # Create your models here.
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)
+    order_number = models.CharField(max_length=40, null=False, editable=False)
     profile = models.ForeignKey(Profile, on_delete=models.SET_NULL,
                                      null=True, blank=True,
                                      related_name='orders')
